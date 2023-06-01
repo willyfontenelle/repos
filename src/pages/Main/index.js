2a7e8c52db1526bf398db4f3eccaf22b57416248
willyfontenelle <willyfontenelle@gmail.com>
2023-06-01 17:18:57 -0300

    final aula 130

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useCallback } from 'react';
-import { FaGithub, FaPlus } from 'react-icons/fa';
+import { FaGithub, FaPlus, FaSpinner } from 'react-icons/fa';
 import { Container, Form, SubmitButton } from './styles';
 
 import api from '../../services/api';
@@ -8,29 +8,37 @@ export default function Main() {
 
     const [newRepo, setNewRepo] = useState('');
     const [repositorios, setRepositorios] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     const handleSubmit = useCallback((e) => {
-        
         e.preventDefault();
-
-        async function submit(){
-            const response = await api.get(`repos/${newRepo}`)
     
-        const data = {
-            name: response.data.full_name,
+        async function submit() {
+          setLoading(true);
+          try {
+            const response = await api.get(`repos/${newRepo}`);
+    
+            const data = {
+              name: response.data.full_name,
+            }
+    
+            setRepositorios([...repositorios, data]);
+            setNewRepo('');
+          } catch (error) {
+            console.log(error);
+          } finally {
+            setLoading(false);
+          }
+    
         }
-
-        setRepositorios([...repositorios, data]);
-        setNewRepo('');
-        }
-
+    
         submit();
+    
+      }, [newRepo, repositorios]);
 
-    }, [newRepo, repositorios]);
-
-    function handleinputChange(e){
+      function handleinputChange(e) {
         setNewRepo(e.target.value);
-    }
+      }
 
     return (
         <Container>
@@ -41,7 +49,7 @@ export default function Main() {
             </h1>
 
 
-            <Form onSubmit={ handleSubmit }>
+            <Form onSubmit={handleSubmit}>
                 <input
                     type="text"
                     placeholder="Adicionar Repositorios"
@@ -49,8 +57,12 @@ export default function Main() {
                     onChange={handleinputChange}
                 />
 
-                <SubmitButton>
-                    <FaPlus color="#FFF" size={14} />
+                <SubmitButton loading={loading ? 1 : 0}>
+                    {loading ? (
+                        <FaSpinner color="#FFF" size={14} />
+                    ) : (
+                        <FaPlus color="#FFF" size={14} />
+                    )}
                 </SubmitButton>
             </Form>
 
